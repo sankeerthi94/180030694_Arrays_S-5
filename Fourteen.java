21358904c07a8f313cabaae6f7f23f538edbc602
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-11 09:20:11 +0530

    Create Fourteen.java

---
+++ Fourteen.java
@@ -0,0 +1,28 @@
+package loops;
+import java.util.*;
+public class fourteen {
+public static void main(String args[])
+	{
+		System.out.println("Enter an integer number ");
+		int num,c=1;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		if(num==0)
+			c=0;
+		boolean flag = false;
+	    for (int i = 2; i <= num / 2; ++i) {
+	      if (num % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+	    
+	    if(c==0)
+	    	System.out.println("0 is neither prime nor composite.");
+	    else if (!flag)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+	}
+
+}
