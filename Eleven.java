1ab9aaa38697914d652e91adc7e1737496596848
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-11 09:18:17 +0530

    Create Eleven.java

---
+++ Eleven.java
@@ -0,0 +1,16 @@
+package loops;
+import java.util.*;
+public class elevenn
+{
+	  public static void main(String []args)
+	  {
+	   //number
+      int n = 20;
+      int even=2;
+      while (even<=n)
+      {
+          System.out.print(even+"  ");
+          even += 2;
+      }
+  }
+}
