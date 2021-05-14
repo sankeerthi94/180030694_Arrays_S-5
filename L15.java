0d319157dc4c31b5a5c49dceeee46062e761ce33
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-14 12:05:36 +0530

    Create L15.java

---
+++ L15.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class L15 {
+public static void main(String[] args) 
+{
+	Scanner sc = new Scanner(System.in);   
+	  System.out.println("Enter the number:");  
+	  String str=sc.nextLine();  
+	   char[] n = str.toCharArray();  
+	   int sum=0;  
+	for(int i =0;i<n.length;i++)  
+	{  
+	   sum = sum + ((int)n[i]);  
+	   sum = sum-48;  
+	}  
+	System.out.println("sum of digits: "+sum);  
+	}
+}
