2458fb9b53a3094e0bb9442f046763e0b9006103
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-14 12:01:33 +0530

    Create L8.java

---
+++ L8.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class L8 {
+
+	public static void main(String[] args) {
+		char colorCode = 'W';
+		switch (colorCode) {
+		case 'R':
+			System.out.println("R->Red");
+			break;
+		case 'G':
+			System.out.println("G->Green");
+			break;
+		case 'B':
+			System.out.println("B->Blue");
+			break;
+		case 'O':
+			System.out.println("O->Orange");
+			break;
+		case 'Y':
+			System.out.println("Y->Yellow");
+			break;
+		case 'W':
+			System.out.println("W->White");
+			break;
+		default:
+			System.out.println("Invalid Code");
+		}
+	}
+}
