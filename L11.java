db3e4010583fa866d5cf77c7a9427425463ca9d6
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-14 12:02:49 +0530

    Create L11.java

---
+++ L11.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class L11
+{
+	  public static void main(String []args)
+	  {
+	   //number
+      int n = 57;
+      int even=23;
+      while (even<=n)
+      {
+          System.out.print(even+"  ");
+          even += 2;
+      }
+  }
+}
