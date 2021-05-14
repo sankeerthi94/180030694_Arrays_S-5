733587c776e78a34e8adc3b08b3bd7e26017ed34
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-14 11:59:58 +0530

    Create L4.java

---
+++ L4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class L4
+{
+	public static void main(String[] args) {
+		char x = 'a';
+		char y = 'e';
+		
+		if (x < y) {
+			System.out.println(x + ", " + y);
+		} else {
+			System.out.println(y + ", " + x);
+		}
+
+	}
+}
