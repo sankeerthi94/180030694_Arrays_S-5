e136c38e9b775736ad2bd7d9a1c37ca31742743b
sankeerthi94 <77227436+sankeerthi94@users.noreply.github.com>
2021-05-14 12:05:57 +0530

    Create L16.java

---
+++ L16.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class L16 {
+
+	  public static void main(String[] args) {
+	    int rows = 3;
+
+	    for (int i = 1; i <= rows; ++i) {
+	      for (int j = 1; j <= i; ++j) {
+	        System.out.print("* ");
+	      }
+	      System.out.println();
+	    }
+	  }
+	}
